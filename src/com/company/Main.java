d32edccb9319d16683440db19c10c3aef9050314
max <maxraadde@hotmail.com>
2016-02-11 17:24:31 +0100

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Main {
 
+    public int classrooms = 7;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Test Push");
