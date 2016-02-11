42741538cc782e007c0ca8a9ecc31b30fa9da3f8
max <maxraadde@hotmail.com>
2016-02-11 18:26:27 +0100

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,30 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public int N_CLASSROOMS = 7;
     public int N_TIME_SLOTS = 5;
     public int N_DAYS = 5;
 
+
+
     public static void main(String[] args) {
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("RoutePPAdvant2.txt"));
+            String str;
+            str = in.readLine();
+            while ((str = in.readLine()) != null) {
+                System.out.println(str);
+            }
+            in.close();
+        } catch (IOException e) {
+            System.out.println("File Read Error");
+        }
 
         System.out.println("Hello World");
         System.out.println("Test Push");
