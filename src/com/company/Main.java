60607e5b2053f81df9432decf78419b3240de0f1
Nicole-de-groot <nicole-de-groot@live.nl>
2016-02-11 17:29:16 +0100

    -

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hello World");
         System.out.println("Test Push");
         System.out.println("Push Back");
+=======
+	    System.out.println("Hello World");
+        System.out.println("Hello Again");
+        System.out.println("Push");
+        System.out.println("Haaay");
+>>>>>>> Stashed changes
     }
 }
