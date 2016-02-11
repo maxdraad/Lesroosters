6cc7a341f0071eee56ba85eff787509f856be59d
max <maxraadde@hotmail.com>
2016-02-11 17:46:15 +0100

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,19 @@ package com.company;
 
 public class Main {
 
-    public int classrooms = 77;
+    public int N_CLASSROOMS = 7;
+    public int N_TIME_SLOTS = 5;
+    public int N_DAYS = 5;
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
+
         System.out.println("Hello World");
         System.out.println("Test Push");
         System.out.println("Push Back");
-=======
 	    System.out.println("Hello World");
         System.out.println("Hello Again");
         System.out.println("Push");
         System.out.println("Haaay");
->>>>>>> Stashed changes
+
     }
 }
