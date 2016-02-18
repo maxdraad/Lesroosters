9cb6d3dd22a2929ff00c5b9e39093e8ccd40f0cf
max <maxraadde@hotmail.com>
2016-02-18 18:13:46 +0100

    CSV DATA FILE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -15,28 +17,28 @@ public class Main {
 
         public static void main(String[] args) {
 
-        // BufferedReader die het tekstbestand met Studentgegevens inleest
+        // In dit gedeelte worden studentgegevens in een List in een ArrayList gezet
+        // (dus een 2x2 tabel met studentgegevens)
         try {
-            BufferedReader in = new BufferedReader(new FileReader("resources/studenten_roostering.csv"));
+            BufferedReader csvStudentGegevens = new BufferedReader(new FileReader("resources/studenten_roostering.csv"));
             String str;
-            str = in.readLine();
-            ArrayList<String> dataList = new ArrayList<String>();
-            while ((str = in.readLine()) != null) {
-                 dataList.add(str);
+            str = csvStudentGegevens.readLine();
+
+            ArrayList<List<String>> studentGegevens = new ArrayList<>();
+
+            while ((str = csvStudentGegevens.readLine()) != null) {
+
+                List<String> gegevens = Arrays.asList(str.split(","));
+                studentGegevens.add(gegevens);
             }
-            System.out.println(dataList);
-            in.close();
+            System.out.println( studentGegevens.get(3) );
+            csvStudentGegevens.close();
         } catch (IOException e) {
             System.out.println("File Read Error");
         }
 
-        System.out.println("Hello World");
-        System.out.println("Test Push");
-        System.out.println("Push Back");
-	    System.out.println("Hello World");
-        System.out.println("Hello Again");
-        System.out.println("Push");
-        System.out.println("Haaay");
-
+        System.out.println("Push test");
+        Student stud1 = new Student("Piet");
+            System.out.println(stud1.name);
     }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Max on 18-2-2016.
+ */
+public class Student {
+    public String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
