f9b0376f3236ef4a50dba12a8e36ac748aaaafd5
Max <maxraadde@hotmail.com>
2016-04-19 16:21:15 +0200

    capaciteit conflict teller werkt!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -226,24 +226,24 @@ public class Main {
             System.out.println("0 punten, niet alle activities zijn ingeroosterd");
             scoreValue = 0;
         }
+
         int studentConflictCounter = 0;
         //Method die studenten met meerdere vakken op een tijdstip telt (dit is aantal maluspunten)
 
-        /* Klopt nog iets niet - geeft altijd 709 als uitkomst.
         int capacityConflictCounter = 0;
-        for (Activity activity : activities){
-            for (Room room : rooms){
-                if (room.capacity < activity.studentGroup.size()){
-                    int numberOfConflict = activity.studentGroup.size()-room.capacity;
-                    for(int i = 0; i < numberOfConflict; i++) {
-                        capacityConflictCounter++;
+        for (Room room : rooms){
+            for (Activity activity : room.timetable){
+                if(activity != null) {
+                    if (room.capacity < activity.studentGroup.size()) {
+                        int numberOfConflict = activity.studentGroup.size() - room.capacity;
+                        capacityConflictCounter = capacityConflictCounter + numberOfConflict;
+                        break;
                     }
-                    break;
                 }
             }
         }
 
-        System.out.println(capacityConflictCounter + " studenten passen niet in hun lokaal!");*/
+        System.out.println(capacityConflictCounter + " studenten passen niet in hun lokaal!");
     }
 
     //Method om activities mee te maken. WERKT :))
